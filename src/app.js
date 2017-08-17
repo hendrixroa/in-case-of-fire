3f2bb750af71e9a06c093d35ef2eb4f0d0fa83be
Hendrix Roa <hendrix.alex87@gmail.com>
2017-08-16 20:12:14 -0400

    delete file renderer and move logic in app of react

--- src/app.js
+++ src/app.js
@@ -1,12 +1,62 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+const { dialog } = window.require('electron').remote
+const app = window.require('electron').remote.app
+const storage = window.require('electron-json-storage')
+const _ = require('lodash')
 
 class Main extends Component {
   constructor(props){
-    super();
+    super()
 
     this.state = {
-    };
+      projects: []
+    }
+
+    storage.getAll((error, data) => {
+      if (error) throw error
+  
+      if(!_.isEmpty(data.projects)){
+        console.log('cargando proyectos', data.projects)
+        this.setState({
+          projects: data.projects
+        })
+      }
+    })
+  }
+
+  addProjects() {
+    const path = dialog.showOpenDialog({
+      properties: ['openDirectory']
+    })
+    console.log('antes de agregar', this.state.projects)
+    let buffer = this.state.projects
+    buffer.push({dir: path})
+    this.setState({
+      projects: buffer
+    })
+    storage.set('projects', this.state.projects , (error) => {
+      if (error) throw error
+    })
+    console.log('Se agregaron estos proyectos', this.state.projects)
+  }
+
+  deleteProjects() {
+    storage.remove('projects', function(error) {
+      if (error) throw error;
+    })
+    this.setState({
+      projects: []
+    })
+  }
+
+  listProjects() {
+    if(this.state.projects.length > 0){
+      return this.state.projects.map((project) => {
+        (<p> project.dir </p>)
+      })
+    }
+    return null;
   }
 
   render(){
@@ -15,15 +65,14 @@ class Main extends Component {
         <div id="empty">
           <p>Empty Projects :(</p>
         </div>
-          <button type="button" id="addProjects">Add One</button>
+          <button type="button" onClick={this.addProjects.bind(this)} id="addProjects">Add One</button>
           <button type="button" id="deleteProjects">delete projects</button>
           <div id="projects">
-        </div>
+            {this.listProjects() ? this.listProjects() : '' }
+          </div>
       </div>
     );
   }
- 
 }
 
-var main = document.getElementById('main');
-ReactDOM.render(<Main />, main);
+ReactDOM.render(<Main />, document.getElementById('main'))
